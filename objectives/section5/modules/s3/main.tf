04cf6a7de458ed8b93f90c9a12394380f5fc2622
Tom Plisson <tplisson@paloaltonetworks.com>
2023-08-03 16:29:07 +0200

    2023-08-03

--- objectives/section5/modules/s3/main.tf
+++ objectives/section5/modules/s3/main.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.10.0"
+      version = ">= 5.08.0"
     }
     random = {
       source  = "hashicorp/random"
